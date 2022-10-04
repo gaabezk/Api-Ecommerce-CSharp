4ae19405067ae1a4466aa54962e16a263d03c9aa
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-04 17:26:45 -0300

    PessoaDTO
    - intalado pacote FluentValidation
    - DTO de pessoa criada
    - Validacao de entrada pela DTO criada

---
+++ com.myapi.Application/DTO/PessoaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace com.myapi.Application.DTO;
+
+public class PessoaDTO
+{
+    public int Id { get; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public string Cpf { get; set; }
+    public string Telefone { get; set; }
+
+}
---
+++ com.myapi.Application/DTO/Validations/PessoaDTOValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace com.myapi.Application.DTO.Validations;
+
+public class PessoaDTOValidator : AbstractValidator<PessoaDTO>
+{
+    public PessoaDTOValidator()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Nome deve ser informado!");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Email deve ser informado!");
+
+        RuleFor(x => x.Senha)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Senha deve ser informado!");
+
+        RuleFor(x => x.Cpf)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("CPF deve ser informado!");
+
+        RuleFor(x => x.Telefone)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Telefone deve ser informado!");
+    }
+}
