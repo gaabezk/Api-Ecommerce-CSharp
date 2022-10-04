660cd02a633dcbd299adb200587536705087d809
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-04 18:41:35 -0300

    Mapping
    - pacote AutoMapper
    - Transformar DTO's em Entidade e vice versa

---
+++ com.myapi.Application/Mappings/DomainToDtoMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using com.myapi.Application.DTO;
+using com.myapi.Domain.Entities;
+
+namespace com.myapi.Application.Mappings;
+
+public class DomainToDtoMapping : Profile
+{
+    public DomainToDtoMapping()
+    {
+        CreateMap<Pessoa, PessoaDTO>();
+    }
+}
---
+++ com.myapi.Application/Mappings/DtoToDomainMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using com.myapi.Application.DTO;
+using com.myapi.Domain.Entities;
+
+namespace com.myapi.Application.Mappings;
+
+public class DtoToDomainMapping : Profile
+{
+    public DtoToDomainMapping()
+    {
+        CreateMap<PessoaDTO, Pessoa>();
+    }
+}
