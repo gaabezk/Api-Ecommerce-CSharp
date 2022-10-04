d6ae294e19b991c9e95581c01c67123c2fdbd490
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-04 18:42:45 -0300

    Pessoa Service
    - Interface IPessoaService
    - Class PessoaService

---
+++ com.myapi.Application/Services/Interfaces/IPessoaService.cs
@@ -0,0 +1,8 @@
+﻿using com.myapi.Application.DTO;
+
+namespace com.myapi.Application.Services.Interfaces;
+
+public interface IPessoaService
+{
+    Task<ResultService<PessoaDTO>> CreateAsync(PessoaDTO pessoaDto);
+}
---
+++ com.myapi.Application/Services/PessoaService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using com.myapi.Application.DTO;
+using com.myapi.Application.DTO.Validations;
+using com.myapi.Application.Services.Interfaces;
+using com.myapi.Domain.Entities;
+using com.myapi.Domain.Repositories;
+
+namespace com.myapi.Application.Services;
+
+public class PessoaService : IPessoaService
+{
+    private readonly IMapper _mapper;
+    private readonly IPessoaRepository _pessoaRepository;
+
+    public PessoaService(IPessoaRepository pessoaRepository, IMapper mapper)
+    {
+        _pessoaRepository = pessoaRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ResultService<PessoaDTO>> CreateAsync(PessoaDTO pessoaDto)
+    {
+        if (pessoaDto == null)
+            return ResultService.Fail<PessoaDTO>("Objeto deve ser informado");
+
+        var result = new PessoaDTOValidator().Validate(pessoaDto);
+        if (!result.IsValid)
+            return ResultService.RequestError<PessoaDTO>("Problemas de validade!", result);
+
+        var pessoa = _mapper.Map<Pessoa>(pessoaDto);
+        var data = await _pessoaRepository.CreateAsync(pessoa);
+        return ResultService.Ok(_mapper.Map<PessoaDTO>(data));
+    }
+}
