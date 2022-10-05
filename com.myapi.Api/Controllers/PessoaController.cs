7226f851fbd0144b3d3a4de7695626470a07efdc
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-04 21:54:35 -0300

    Controller pessoa + FIX
    - getAll
    - post

---
+++ com.myapi.Api/Controllers/PessoaController.cs
@@ -0,0 +1,37 @@
+﻿using com.myapi.Application.DTO;
+using com.myapi.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace com.myapi.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PessoaController : ControllerBase
+{
+    private readonly IPessoaService _pessoaService;
+
+    public PessoaController(IPessoaService pessoaService)
+    {
+        _pessoaService = pessoaService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> PostAsync([FromBody] PessoaDTO pessoaDto)
+    {
+        var result = await _pessoaService.CreateAsync(pessoaDto);
+        if (result.IsSuccess)
+            return Ok(result);
+
+        return BadRequest(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllAsync()
+    {
+        var result = await _pessoaService.GetAllAsync();
+        if (result.IsSuccess)
+            return Ok(result);
+
+        return BadRequest(result);
+    }
+}
--- com.myapi.Application/DTO/PessoaDTO.cs
+++ com.myapi.Application/DTO/PessoaDTO.cs
@@ -2,7 +2,7 @@
 
 public class PessoaDTO
 {
-    public int Id { get; }
+    public int Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
     public string Senha { get; set; }
