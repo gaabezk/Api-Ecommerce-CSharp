5ef09ebd46711aa130801c961470d2c8f1e638d3
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-04 21:03:28 -0300

    Injeção de dependência

---
+++ com.myapi.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using com.myapi.Application.Mappings;
+using com.myapi.Application.Services;
+using com.myapi.Application.Services.Interfaces;
+using com.myapi.Domain.Repositories;
+using com.myapi.Infra.Data.Context;
+using com.myapi.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace com.myapi.Infra.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<MyApiContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IPessoaRepository, PessoaRepository>();
+        return services;
+    }
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(typeof(DomainToDtoMapping));
+        services.AddScoped<IPessoaService, PessoaService>();
+        return services;
+    }
+}
