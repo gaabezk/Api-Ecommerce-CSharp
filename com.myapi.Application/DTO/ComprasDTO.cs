6973325314ae84ff1660a12c9cb6b8ef122281af
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-14 18:05:57 -0300

    DTO de Compra e suas Validaçoes

---
+++ com.myapi.Application/DTO/ComprasDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace com.myapi.Application.DTO;
+
+public class ComprasDTO
+{
+    public string CodigoErp { get; set; }
+    public string Cpf { get; set; }
+    public int Id { get; set; }
+}
---
+++ com.myapi.Application/DTO/Validations/ComprasDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace com.myapi.Application.DTO.Validations;
+
+public class ComprasDTOValidator : AbstractValidator<ComprasDTO>
+{
+    public ComprasDTOValidator()
+    {
+        RuleFor(x => x.CodigoErp)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Codigo Erp deve ser informado!");
+        
+        RuleFor(x => x.Cpf)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Cpf deve ser informado!");
+    }
+}
