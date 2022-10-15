b2004eaa23996e1f93e95b99f7e64ea92565e01e
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-14 21:17:18 -0300

    Repositorio de Compra

---
+++ com.myapi.Domain/Repositories/ICompraRepository.cs
@@ -0,0 +1,14 @@
+﻿using com.myapi.Domain.Entities;
+
+namespace com.myapi.Infra.Data.Repositories;
+
+public interface ICompraRepository
+{
+    Task<Compra> GetByIdAsync(int id);
+    Task<ICollection<Compra>> GetAllAsync();
+    Task<Compra> CreateAsync(Compra compras);
+    Task EditAsync(Compra compra);
+    Task DeleteAsync(Compra compra);
+    Task<ICollection<Compra>> GetByPessoaIdAsync(int pessoaId);
+    Task<ICollection<Compra>> GetByProdutoIdAsync(int produtoId);
+}
---
+++ com.myapi.Infra.Data/Repositories/ComprasRepository.cs
@@ -0,0 +1,68 @@
+﻿using com.myapi.Domain.Entities;
+using com.myapi.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace com.myapi.Infra.Data.Repositories;
+
+public class CompraRepository : ICompraRepository
+{
+    private readonly MyApiContext _db;
+
+    public CompraRepository(MyApiContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Compra> CreateAsync(Compra compra)
+    {
+        _db.Add(compra);
+        await _db.SaveChangesAsync();
+        return compra;
+    }
+
+    public async Task DeleteAsync(Compra compra)
+    {
+        _db.Remove(compra);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task EditAsync(Compra compra)
+    {
+        _db.Update(compra);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<Compra> GetByIdAsync(int id)
+    {
+        var compra = await _db.Compras
+            .Include(x => x.Produto)
+            .Include(x => x.Pessoa)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        return compra;
+    }
+
+    public async Task<ICollection<Compra>> GetByPessoaIdAsync(int pessoaId)
+    {
+        return await _db.Compras
+            .Include(x => x.Produto)
+            .Include(x => x.Pessoa)
+            .Where(x => x.PessoaId == pessoaId).ToListAsync();
+    }
+
+    public async Task<ICollection<Compra>> GetByProdutoIdAsync(int produtoId)
+    {
+        return await _db.Compras
+            .Include(x => x.Produto)
+            .Include(x => x.Pessoa)
+            .Where(x => x.ProdutoId == produtoId).ToListAsync();
+    }
+
+    public async Task<ICollection<Compra>> GetAllAsync()
+    {
+        return await _db.Compras
+            .Include(x => x.Produto)
+            .Include(x => x.Pessoa)
+            .ToListAsync();
+    }
+}
