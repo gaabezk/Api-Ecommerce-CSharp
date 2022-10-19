aa40e5eb56fe51ffef2e6d2900e4abd38021a9d7
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-10-19 15:16:29 -0300

    Dto e mapping de COMPRA

---
+++ com.myapi.Application/DTO/CompraDetalheDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace com.myapi.Application.DTO;
+
+public class CompraDetalheDTO
+{
+    public int Id { get; set; }
+    public string Pessoa { get; set; }
+    public string Produto { get; set; }      
+    public DateTime Data { get; set; }      
+}
--- com.myapi.Application/Mappings/DomainToDtoMapping.cs
+++ com.myapi.Application/Mappings/DomainToDtoMapping.cs
@@ -10,6 +10,19 @@ public class DomainToDtoMapping : Profile
     {
         CreateMap<Pessoa, PessoaDTO>();
         CreateMap<Produto, ProdutoDTO>();
-        CreateMap<Compra, CompraDTO>();
+        CreateMap<Compra, CompraDetalheDTO>()
+            .ForMember(x => x.Pessoa, opt => opt.Ignore())
+            .ForMember(x => x.Produto, opt => opt.Ignore())
+            .ConstructUsing((model, context) =>
+            {
+                var dto = new CompraDetalheDTO()
+                {
+                    Produto = model.Produto.Nome,
+                    Id = model.Id,
+                    Data = model.Date,
+                    Pessoa = model.Pessoa.Nome
+                };
+                return dto;
+            });
     }
 }
